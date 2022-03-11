aa52f368cddaf660475c68923dcd8abebfaa24ba
frekeB <61983733+frekeB@users.noreply.github.com>
2022-03-11 18:52:09 +0100

    Romans to decimal function

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,37 @@
-function romanToDecimal(roman) {}
+const converter = {
+  1: "I",
+  4: "IV",
+  5: "V",
+  9: "IX",
+  10: "X",
+  40: "XL",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M"
+};
+const numbers = [1000, 900, 500, 400, 100, 50, 40, 10, 9, 5, 4, 1];
 
-module.exports = romanToDecimal
+function romanToDecimal(roman) {
+  let result = "";
+  let i = 0;
+  while (roman > 0) {
+    const subNum = numbers[i];
+    if (subNum > roman) {
+      i++;
+    } else {
+      roman -= subNum;
+      result += converter[subNum];
+    }
+  }
+  return result;
+}
+
+console.log(romanToDecimal(36));
+
+module.exports = romanToDecimal;
+
+module.exports = romanToDecimal;
